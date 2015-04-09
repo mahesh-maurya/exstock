2ddd1b63f8960418cae06b1c5a4ead5c40c3891f
mahesh-wohlig <mahesh@wohlig.com>
2015-04-09 15:17:53 +0530

    done

--- js/app.js
+++ js/app.js
@@ -82,5 +82,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         })
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/signin');
 });
